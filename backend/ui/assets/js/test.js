340368008d764de48c97bc91b87535e8a8c3a35e
Plcuf <romeo.sanchez@ynov.com>
2024-06-18 09:21:57 +0200

    feat: nav_left dropdown & nav_right (html/css)

---
+++ backend/ui/assets/js/test.js
@@ -0,0 +1,23 @@
+const has_list = document.querySelectorAll('.has-list');
+
+function refresh_height() {
+    const category_list = document.querySelectorAll('.category-list');
+    
+    category_list.forEach((list) => {
+        list.style.maxHeight = `${list.children.length * 42}px`; 
+
+        console.log(list.offsetHeight);
+        console.log(list.children.length * 42);
+    });
+}
+
+has_list.forEach((item) => {
+    const item_arrow = item.lastElementChild;
+    item.addEventListener('click', () => {
+        const list = document.querySelector(`#${item.dataset.list}`);
+        list.classList.toggle('closed');
+        item_arrow.classList.toggle('rotated');
+    });
+});
+
+refresh_height();
